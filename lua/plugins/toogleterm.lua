af8eb2b3f83d927ae4327d6bd39c578977b6f8ff
Fabrizio Pomata <fabrizio.pomata@cbi.com.py>
2023-11-27 08:12:27 -0300

    Initial commit

---
+++ lua/plugins/toogleterm.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "akinsho/toggleterm.nvim",
+    opts = {
+      direction = "horizontal",
+      open_mapping = "<leader>td",
+      insert_mappings = false,
+    },
+    float_opts = {
+      border = "single",
+    },
+    version = "*",
+    config = true,
+  },
+}
